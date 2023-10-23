bbdec5739b691ced7863d3a248a951edbbe703a9
dariabilokin <dariabilokin@gmail.com>
2023-10-23 14:05:55 +0300

    add gitignore file

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,24 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {
+    "/": [
+      "static/media/34a9823ac715e2c2-s.p.woff2",
+      "static/media/ae0a0c671023eecc-s.p.woff2"
+    ],
+    "/sale/[saleId]": [
+      "static/media/34a9823ac715e2c2-s.p.woff2"
+    ]
+  },
+  "app": {
+    "/Users/dariabilokin/Projects/movingout/node_modules/next/dist/build/webpack/loaders/css-loader/src/index.js??ruleSet[1].rules[1].oneOf[3].use[1]!/Users/dariabilokin/Projects/movingout/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js??ruleSet[1].rules[1].oneOf[3].use[2]!/Users/dariabilokin/Projects/movingout/node_modules/next/font/google/target.css?{\"path\":\"components/shared/Footer/Footer.js\",\"import\":\"Quicksand\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"quicksand\"}": [
+      "static/media/34a9823ac715e2c2-s.p.woff2"
+    ],
+    "/Users/dariabilokin/Projects/movingout/node_modules/next/dist/build/webpack/loaders/css-loader/src/index.js??ruleSet[1].rules[1].oneOf[3].use[1]!/Users/dariabilokin/Projects/movingout/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js??ruleSet[1].rules[1].oneOf[3].use[2]!/Users/dariabilokin/Projects/movingout/node_modules/next/font/google/target.css?{\"path\":\"pages/index.js\",\"import\":\"League_Spartan\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"leagueSpartan\"}": [
+      "static/media/ae0a0c671023eecc-s.p.woff2"
+    ],
+    "/Users/dariabilokin/Projects/movingout/node_modules/next/dist/build/webpack/loaders/css-loader/src/index.js??ruleSet[1].rules[1].oneOf[3].use[1]!/Users/dariabilokin/Projects/movingout/node_modules/next/dist/build/webpack/loaders/next-font-loader/index.js??ruleSet[1].rules[1].oneOf[3].use[2]!/Users/dariabilokin/Projects/movingout/node_modules/next/font/google/target.css?{\"path\":\"pages/index.js\",\"import\":\"Quicksand\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"quicksand\"}": [
+      "static/media/34a9823ac715e2c2-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": true
+}
